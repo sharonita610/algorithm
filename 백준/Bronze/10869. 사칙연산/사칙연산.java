e7e0f014db06db9b3a85a8a748a21a70ff50f689
Sharon Kim <96200501+sharonita610@users.noreply.github.com>
2023-04-05 22:43:22 +0900

    [Bronze V] Title: 사칙연산, Time: 212 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,18 @@
+  import java.util.*;
+    public class Main{
+        public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+            
+                long a, b;
+                a = sc.nextInt();
+                b = sc.nextInt();
+            
+            System.out.println(a+b);
+            System.out.println(a-b);
+            System.out.println(a*b);
+            System.out.println(a/b);
+            System.out.println(a%b);
+           
+
+        }
+    }
