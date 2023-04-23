37642d53f6222db8036433e8056011f493f6cb4b
Sharon Kim <96200501+sharonita610@users.noreply.github.com>
2023-04-24 00:29:55 +0900

    [Bronze V] Title: 오늘 날짜, Time: 116 ms, Memory: 13888 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        
+        System.out.println("2023-04-24");
+        
+    }
+}
