03630df4f9ff40f2f8884d64a958744a30bc8495
Sharon Kim <96200501+sharonita610@users.noreply.github.com>
2023-04-25 21:21:25 +0900

    [Bronze V] Title: A+B - 8, Time: 264 ms, Memory: 18912 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]){
+
+        int n, a, b;
+
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+
+            a = sc.nextInt();
+            b = sc.nextInt();
+
+            System.out.println("Case #"+ i + ": "+ a +" + "+ b + " = " +(a + b ));
+        }
+        sc.close();
+    }
+}
