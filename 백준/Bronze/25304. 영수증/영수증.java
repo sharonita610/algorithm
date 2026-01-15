6818aa7ccc75823a4777c4d24e026bf32f9febb7
Sharon Kim <96200501+sharonita610@users.noreply.github.com>
2026-01-16 03:51:42 +0900

    [Bronze IV] Title: 영수증, Time: 180 ms, Memory: 17856 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.java
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -1,26 +1,23 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String args[]) {
 
-        int totPri, items, price, unit, total = 0;
-        Scanner sc = new Scanner(System.in);
-        totPri = sc.nextInt();
-        items = sc.nextInt();
-        for (int i = 0; i < items; i++) {
-            price = sc.nextInt();
-            unit = sc.nextInt();
-            total += (price*unit);
-        }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int totalPrice = sc.nextInt();
+		int totalCount = sc.nextInt();
 
-       
-        if(totPri == total){
-            System.out.print("Yes");
-        }else {
-            System.out.print("No");
+		for (int i = 1; i <= totalCount; i++) {
+			int unitPrice = sc.nextInt();
+			int unitCount = sc.nextInt();
+			totalPrice -= unitPrice * unitCount;
+		}
 
-        }
-        sc.close();
-
-    }
+		if (totalPrice == 0) {
+			System.out.println("Yes");
+		} else{
+			System.out.println("No");
+		}
+		sc.close();
+	}
 }
