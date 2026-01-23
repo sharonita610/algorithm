c655492ca522dfa32c2e75953316be59c501a5cb
Sharon Kim <96200501+sharonita610@users.noreply.github.com>
2026-01-23 17:02:13 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.49 ms, Memory: 71.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] array, int height) {
+        int answer = 0;
+        Arrays.sort(array);
+        
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] > height) {
+                answer += 1;
+            }
+        }
+        
+        return answer;
+    }
+}
