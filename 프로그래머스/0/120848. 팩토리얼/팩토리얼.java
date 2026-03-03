b786fb7915eebd1c1d5afe2e55dfe08a11107c6f
Sharon Kim <96200501+sharonita610@users.noreply.github.com>
2026-03-04 02:29:41 +0900

    [level 0] Title: 팩토리얼, Time: 0.02 ms, Memory: 90.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+         int answer = 1;
+        int i = 1;
+
+        while(answer <= n) {
+            i++;
+            answer *= i;
+        }
+        return i - 1;
+    }
+}
