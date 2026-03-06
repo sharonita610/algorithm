8f2c25b0f9b23ae132b0b2e19a4ca5af2a167672
Sharon Kim <96200501+sharonita610@users.noreply.github.com>
2026-03-07 04:22:45 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 4.86 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public long solution(long n) {
+        String collect = Arrays.stream(String.valueOf(n).split(""))
+                .sorted(Collections.reverseOrder())
+                .collect(Collectors.joining());
+
+        return Long.parseLong(collect);
+    }
+}
